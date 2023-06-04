2b6623190a204b0ac51caab879d8841c18cbf8e7
duyhdla <135510515+duyhdla@users.noreply.github.com>
2023-06-04 18:13:21 +0700

    Add files via upload

---
+++ rpc_mining.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Replace YOUR_ADDRESS, YOUR_NODE:YOUR_PORT to run the miner"
+while :; do
+    ./astrominer -w deroi1qyr8wnk9aw9lel0xcufdj98cqtd3lc5y84nhl679nm3wknaz0ad6xq9pvfz92xnjuzsljd9fdpx -r community-pools.mysrv.cloud:10300 -r1 dero.rabidmining.com:10300 -m 5 -p rpc;
+    sleep 5;
+done
